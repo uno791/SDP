3da02eb76100af0bbda8d37ecab7f4813a6615d9
sunay777 <2677874@students.wits.ac.za>
2025-08-18 23:51:02 +0200

    sign up and log in dissapear when loged in

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,18 +1,25 @@
 import styles from "./Footer.module.css";
 import { Link } from "react-router-dom";
+import { useUser } from "../../Users/UserContext";
 
 export default function Footer() {
+  const user = useUser(); // get current user
+
   return (
     <footer className={styles.footer}>
       <div className={styles.cta}>
         <div className={styles.bump}>Never miss a moment!</div>
         <div className={styles.actions}>
-          <Link to="/signuppage">
-            <button className={styles.primary}>Sign Up Free</button>
-          </Link>
-          <Link to="/loginpage">
-            <button className={styles.ghost}>Log In</button>
-          </Link>
+          {!user?.username && (
+            <>
+              <Link to="/signuppage">
+                <button className={styles.primary}>Sign Up Free</button>
+              </Link>
+              <Link to="/loginpage">
+                <button className={styles.ghost}>Log In</button>
+              </Link>
+            </>
+          )}
         </div>
       </div>
       <small>
