d6b57e7f4b3a5b39b522070aef80f75b284dfa45
sunay777 <2677874@students.wits.ac.za>
2025-08-18 23:39:50 +0200

    username and navbar

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,15 +1,21 @@
 import styles from "./Header.module.css";
+import { useUser } from "../../Users/UserContext";
+import { Link } from "react-router-dom";
 
 export default function Header() {
+  const u = useUser();
+  const username = u?.username || "Guest"; // fallback if username is undefined
+
   return (
     <header className={styles.nav}>
       <div className={styles.brand}>Foot Book</div>
       <nav className={styles.links}>
-        <a className={styles.live}>LIVE</a>
-        <a>Home</a>
-        <a>News</a>
-        <a>Favourite Teams</a>
-        <a>Profile</a>
+        <span className={styles.live}>LIVE</span>
+        <Link to="/">Home</Link>
+        <span>News</span>
+        <span>Favourite Teams</span>
+        <Link to="/doodlehome">doodleHome</Link>
+        <span className={styles.username}>{username}</span>
       </nav>
     </header>
   );
